799b6fd6f984ba970bedcb7835dbaeff6744225b
LucasGabrielGit <36990357+LucasGabrielGit@users.noreply.github.com>
2020-07-24 14:48:58 -0300

    Algumas alterações

--- src/app/template/nav-bar/nav-bar.component.ts
+++ src/app/template/nav-bar/nav-bar.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html'
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar-component.css'],
 })
 export class NavBarComponent implements OnInit {
+  nome:string = 'Lucas';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
